ec6b10c4e737cd455b8d6d4f1eb25a01533fd750
Alex <alex.williams1@my.jcu.edu.au>
2024-06-10 23:44:00 +1000

    Update password check program

--- password_stars.py
+++ password_stars.py
@@ -1,8 +1,6 @@
-PASSWORD = "Pythonista"
-
-
+password_length = 10
 password = input("Enter your password: ")
-while len(password) < len(PASSWORD):
-    print(f"Password must be at least {len(PASSWORD)} long.")
+while len(password) < password_length:
+    print(f"Password must be at least {password_length} long.")
     password = input("Enter your password: ")
-print(f"{'*' * len(PASSWORD)}")
+print(f"{'*' * len(password)}")
